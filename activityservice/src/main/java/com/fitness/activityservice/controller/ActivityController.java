3e9a4beb04851b2738a3c1b7f28144121384cb78
thurunuym <marasinghe.ty@gmail.com>
2025-09-07 22:12:11 +0530

    Activity endpoints

--- activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
+++ activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -8,17 +8,24 @@ import com.fitness.activityservice.service.ActivityService;
 
 import com.fitness.activityservice.dto.ActivityRequest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("api/activities")
 public class ActivityController {
 
-    @Autowired            //Spring finds the ActivityService bean and injects it.
-    ActivityService activityService;
+    @Autowired
+    private ActivityService activityService;
 
     @PostMapping
     public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request) {
@@ -26,5 +33,28 @@ public class ActivityController {
         return ResponseEntity.ok(activityService.trackActivity(request));    
     
     }
+
+    @GetMapping
+    public ResponseEntity<List<ActivityResponse>> getUserActivities(@RequestHeader("X-User-ID") String userId){
+
+            return ResponseEntity.ok(activityService.getUserActivities(userId));
+        
+        }
+    
+    
+    //It expects a request header named "X-User-ID".
+    //The value of that header is captured into the userId parameter.
+    //Key: X-User-ID
+    //Value: 222222
+
+
+
+    @GetMapping("/{activityId}")
+    public ResponseEntity<ActivityResponse> getActivities(@PathVariable("activityId") String activity){
+
+            return ResponseEntity.ok(activityService.getActivity(activity));
+        
+        }
+
     
 }
