dcbf75cab24cd5f17a2c995307e358b4f76e4c81
thurunuym <marasinghe.ty@gmail.com>
2025-09-10 17:07:47 +0530

    user validation

--- activityservice/src/main/java/com/fitness/activityservice/service/UserValidationService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/UserValidationService.java
@@ -24,10 +24,26 @@ public class UserValidationService {
                 throw new RuntimeException("User Not Found" + userId);
             else if(e.getStatusCode() == HttpStatus.BAD_REQUEST)
                 throw new RuntimeException("Invalid Request: " + userId);
-            
-            
+                        
             }
         
             return false;
     }
 }
+
+
+//This service Ask user-service if this userId is valid. If not, stop right here.
+
+//WebClient userServiceWebClient is the bean you defined earlier in WebClientConfig, 
+//pointing to USER-SERVICE through Eureka.
+
+//userServiceWebClient.get() → start an HTTP GET request.
+
+//.uri("/api/users/{userId}/validate", userId) → build the URI with path variable substitution.
+//   ex: http://USER-SERVICE/api/users/222222/validate
+
+//.retrieve() → send the request and prepare to extract the response.
+
+//.bodyToMono(Boolean.class) → expect the response body to be a Boolean (true or false).
+
+//.block() → turn the reactive Mono<Boolean> into a normal boolean (synchronous).
