84e8c9d3d50cbe26410bfb88e141e98da96f77de
thurunuym <marasinghe.ty@gmail.com>
2025-09-12 23:36:46 +0530

    commit

--- activityservice/src/main/java/com/fitness/activityservice/service/UserValidationService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/UserValidationService.java
@@ -6,13 +6,16 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserValidationService {
     private final WebClient userServiceWebClient;
 
     public boolean validateUser(String userId){
+        log.info("Calling User Validation API for userId: {}", userId);
         try{
             return userServiceWebClient.get()
                 .uri("/api/users/{userId}/validate", userId)
