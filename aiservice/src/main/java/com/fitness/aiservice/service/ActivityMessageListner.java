c9f7d6f2065a2ee41ab675c6a2ebb4addf40d58b
thurunuym <marasinghe.ty@gmail.com>
2025-09-16 04:07:52 +0530

    recommendation is saved in the db

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListner.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListner.java
@@ -4,6 +4,8 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,10 +16,12 @@ import lombok.extern.slf4j.Slf4j;
 public class ActivityMessageListner {
 
     private final ActivityAIService activityAIService;
+    private final RecommendationRepository repo;
 
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity){
         log.info("Received activity for AI processing: {}" , activity.getId());
-        log.info("Generated Recommendation: {}", activityAIService.generateRecommendation(activity));
+        Recommendation recommendation = activityAIService.generateRecommendation(activity);
+        repo.save(recommendation);
     }
 }
